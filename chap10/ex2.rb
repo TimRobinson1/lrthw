82ecd2b837bf57b92fb5c4f4c056143ec6bf725b
Timothy Robinson <timmyrobby@gmail.com>
2016-12-26 03:27:56 +0000

    Chapter 10 completed

---
+++ chap10/ex2.rb
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I\m \\ a \\ cat."
+
+#Using single quotations is a little easier to read/quickly identify.
+fat_cat = '''
+I\'ll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
