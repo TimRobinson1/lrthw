fb797bff6a56c549fde9cca607dc2f14271d0083
Timothy Robinson <timmyrobby@gmail.com>
2016-12-26 04:20:37 +0000

    Added chapter 13

---
+++ chap13/ex2.rb
@@ -0,0 +1,5 @@
+#Study Drills - testing with fewer variables
+meat, fruit = ARGV
+
+puts "Your meat of choice is: #{meat}"
+puts "Your fruit of choice is: #{fruit}"
---
+++ chap13/ex3.rb
@@ -0,0 +1,9 @@
+#Study Drills - testing with fewer variables
+meat, fruit = ARGV
+
+print "What is the colour of life? "
+colour = $stdin.gets.chomp
+
+puts "Your meat of choice is: #{meat}"
+puts "Your fruit of choice is: #{fruit}"
+puts "You think that the colour of life is #{colour}"
