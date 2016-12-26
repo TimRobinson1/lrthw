a17eed9a843525a13cc922b9b1af562592a6e39a
Timothy Robinson <timmyrobby@gmail.com>
2016-12-26 02:24:25 +0000

    Chapter 7 completed

---
+++ chap07/ex2.rb
@@ -0,0 +1,31 @@
+#Study Drills
+
+#Prints string
+puts "Mary had a little lamb."
+
+#Prints string
+puts "Its fleece was white as #{'snow'}."
+
+#Prints string
+puts "And everywhere that Mary went."
+
+#Prints string and multiplies that string by 10, in this case producing 10 periods.
+puts "." * 10 #What'd that do?
+
+#List of variables being defined.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# The difference between these commands is that puts adds a new line after the command, whereas print does not, which leads to the puts command in this case appearing immediately after the print command on the same line.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
