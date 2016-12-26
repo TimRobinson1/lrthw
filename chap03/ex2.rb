5e91448bc10222a7fc43f4709226665bf92c98bc
Tim Robinson <timrobinson@Tims-MacBook-Pro.local>
2016-12-26 00:36:18 +0000

    Chapter 3 completed

---
+++ chap03/ex2.rb
@@ -0,0 +1,11 @@
+#Study Drill 3
+
+puts "I will now add these resources:"
+
+puts "Food cans: #{15 + 3 + 9}"
+puts "Batteries: #{1 + 4}"
+puts "Water: #{57.0 - 3.5} litres."
+
+puts "I have 58 rations of perishable food, but about half has already rotted!"
+
+puts "Edible food: #{58 / 2}"
