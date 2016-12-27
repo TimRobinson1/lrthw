d10448a41da0c6df0a5cd262d4a5c3b682fa94dc
Timothy Robinson <timmyrobby@gmail.com>
2016-12-27 17:18:45 +0000

    Added a few notes to first file inspection

--- chap23/ex1.rb
+++ chap23/ex1.rb
@@ -1,8 +1,13 @@
+# Chapter 23 from Learn Ruby the Hard Way suggests pulling a ruby file from an online hub and examining it.  This file was retrieved from github.com/Duke-PL-Course/Ruby/blob/master/assignments/q1-fib.rb
+
+# Function called fib
 def fib(n, n1 = 1, n2 = 1)
+  # If statements.  Unfamiliar with them in Ruby, but familiar with the general concept of the logic statement.
   if n <= 2
     [0, n1, n2][n]
   else
     ans = nil
+    # Have not seen the 'do' command before.  Research suggests that do is for larger, slightly more complex lines of workings out.
     (n-2).times do
       ans = n1 + n2
       n1 = n2
@@ -14,6 +19,7 @@ end
 
 def fibInject(n, n1 = 1, n2 = 1)
   if n <= 2
+    # Appears to be an array.  Not familiar with arrays in Ruby, but in Javascript.
     [0, n1, n2][n]
   else
     ([nil].cycle.take(n-2).inject([n1, n2]) do |prev, x|
