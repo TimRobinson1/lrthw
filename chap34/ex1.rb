45f336dee46d529c7444bab8c270ea53a2409e07
Timothy Robinson <timmyrobby@gmail.com>
2016-12-28 05:04:26 +0000

    Chapter 34 - retrieving array values

---
+++ chap34/ex1.rb
@@ -0,0 +1,27 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# This is responding to the practice session in Chapter 34 of the book.
+
+puts "The animal at 1."
+first = animals[1] ; puts first
+
+puts "The third (3rd) animal."
+second = animals[2] ; puts second
+
+puts "The first (1st) animal."
+third = animals[0] ; puts third
+
+puts "The animal at 3."
+fourth = animals[3] ; puts fourth
+
+puts "The fifth (5th) animal."
+fifth = animals[4] ; puts fifth
+
+puts "The animal at 2."
+sixth = animals[2] ; puts sixth
+
+puts "The sixth (6th) animal."
+seventh = animals[5] ; puts seventh
+
+puts "The animal at 4."
+eighth = animals[4] ; puts eighth
